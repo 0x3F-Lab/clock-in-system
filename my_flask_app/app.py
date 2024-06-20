aedad2a7c37cb19891a9280b7da9b53626ed3d8f
Mitan <martinmitanoski@gmail.com>
2024-06-20 14:18:00 +0800

    add login page

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for
 from routes.employee import employee_bp
 
 app = Flask(__name__)
 
+# Register Blueprints
 app.register_blueprint(employee_bp, url_prefix='/employee')
+
+@app.route('/')
+def index():
+    return redirect(url_for('employee.login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
