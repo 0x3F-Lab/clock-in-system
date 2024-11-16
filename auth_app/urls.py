0d436b859be81e1f72a27f81dd95747e7adbac77
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-11-16 17:13:22 +0800

    Fix linting errors found with black.

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),  # Set the login view as the default for `auth/`
+    path(
+        "", views.login_view, name="login"
+    ),  # Set the login view as the default for `auth/`
 ]
