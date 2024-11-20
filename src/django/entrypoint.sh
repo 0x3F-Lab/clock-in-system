2bc0f786091ab96a234d741eba855bc33f588733
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-11-21 02:34:49 +0800

    Updated project structure to use postgres - Add postgres to project using docker containers. - Add container for django app with dockerfile and env files. - Relocate files and folders to match new dockerised setup. - Update django settings to use new postgres database. - Add init file for postgres database for any initial actions to be   taken.

---
+++ src/django/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Run database migrations
+echo "Running database migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+# Start the Django development server
+echo "Starting Django development server..."
+exec "$@"
