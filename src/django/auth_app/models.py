2c54c830f218e399eb73272a96abab34b8b60ec2
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-11-28 22:15:31 +0800

    Add initial database models and fix configuration - Update pre-commit to ignore migration files. - Update docker compose to ensure database runs first.

--- src/django/auth_app/models.py
+++ src/django/auth_app/models.py
@@ -4,18 +4,18 @@ from django.db import models
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100, null=False)
+    email = models.EmailField(unique=True, null=False)
+    pin = models.CharField(
         max_length=256
-    )  # Store hashed passwords -- Allow nullable pass for fresh account (shouldnt be used until password is set)
-    birth_date = models.DateField(blank=True, default=None)
-    is_active = models.BooleanField(default=False)
-    is_manager = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    clocked_in = models.BooleanField(default=False)
+    )  # Store hashed pins -- Allow nullable pin for fresh account (shouldnt be used until password is set)
+    birth_date = models.DateField(blank=True, default=None, null=True)
+    is_active = models.BooleanField(default=False, null=False)
+    is_manager = models.BooleanField(default=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+    clocked_in = models.BooleanField(default=False, null=False)
 
     def __str__(self):
         return self.name
--- src/django/auth_app/views.py
+++ src/django/auth_app/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+from .forms import NewUserForm
+from django.db import IntegrityError
 
 
 def login_view(request):
     return render(request, "auth_app/login.html")
+
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, "auth_app/user_list.html", {"users": users})
+
+
+def create_user(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect("user_list")
+            except IntegrityError as e:
+                print(f"Database integrity error when adding new user: {e}")
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+
+    else:
+        form = NewUserForm()
+    return render(request, "auth_app/create_user.html", {"form": form})
