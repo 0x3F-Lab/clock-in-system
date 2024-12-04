050d6f28def3e85241dea7fbf8ac9abb2fd8854a
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-12-04 19:04:31 +0800

    Fix importation error

--- src/django/auth_app/views.py
+++ src/django/auth_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required, redirect
+from django.contrib.auth.decorators import login_required
 from auth_app.models import User, Activity
 from auth_app.forms import NewUserForm
 from django.db import IntegrityError
