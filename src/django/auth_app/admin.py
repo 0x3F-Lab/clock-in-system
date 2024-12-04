58c549b8e4d29417c2304ba13ae559142184fb28
Martin <martinmitanoski@gmail.com>
2024-12-05 02:38:21 +0800

    Add model for employee passcode

--- src/django/auth_app/admin.py
+++ src/django/auth_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from auth_app.models import User, Activity, Summary
+from auth_app.models import User, Activity, Summary, KeyValueStore
 
 # Register models
 admin.site.register(User)
 admin.site.register(Activity)
 admin.site.register(Summary)
+admin.site.register(KeyValueStore)
--- src/django/auth_app/models.py
+++ src/django/auth_app/models.py
@@ -64,3 +64,12 @@ class Summary(models.Model):
         return (
             f"[{self.id}] Weekly Summary for {self.employee_id} on {self.summary_date}"
         )
+
+
+class KeyValueStore(models.Model):
+    id = models.AutoField(primary_key=True)
+    key = models.CharField(max_length=255, unique=True)
+    value = models.TextField()
+
+    def __str__(self):
+        return f"{self.key}: {self.value}"
