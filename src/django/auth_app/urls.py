ab9a11d92270b39d36d3083f322bbecdded59a93
AkhilG4 <akhilgorasia@gmail.com>
2024-12-09 13:37:00 +0800

    Add logout button to manager dashboard

--- src/django/auth_app/urls.py
+++ src/django/auth_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", views.login_view, name="home"),
     path("login/", views.login_view, name="login"),
     path("manager_login/", views.manager_login, name="manager_login"),
+     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
     path("manager_dashboard/", views.manager_dashboard, name="manager_dashboard"),
     path("employee_login/", views.employee_login, name="employee_login"),
     path("employee_dashboard/", views.employee_dashboard, name="employee_dashboard"),
