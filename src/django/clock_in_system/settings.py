21560b5f20a18e4cbfcb7b384d911749a0b93a5b
Martin <martinmitanoski@gmail.com>
2025-01-10 13:56:51 +0800

    Fix trusted origins error for django 4.0

--- src/django/clock_in_system/settings.py
+++ src/django/clock_in_system/settings.py
@@ -57,7 +57,10 @@ CSRF_COOKIE_SECURE = str_to_bool(
 )  # Use True in production to send cookies over HTTPS only
 CSRF_COOKIE_HTTPONLY = False  # Default is False; True prevents JavaScript access --- Currently our JS access CSRF
 CSRF_COOKIE_SAMESITE = "Strict"  # Can be 'Lax', 'Strict', or 'None'
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "localhost").split(",")
+CSRF_TRUSTED_ORIGINS = [
+    f"http://{origin}" if not origin.startswith(("http://", "https://")) else origin
+    for origin in os.getenv("CSRF_TRUSTED_ORIGINS", "http://localhost").split(",")
+]
 
 
 # Application definition
