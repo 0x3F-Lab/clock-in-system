e3796052c3bbf27e19718a2aee0f489b4cb22173
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2025-04-13 00:41:57 +0800

    Remove unused base URL path and add API versioning - Set API version to 1 to be able to distinguish version in the future.

--- src/django/clock_in_system/urls.py
+++ src/django/clock_in_system/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),
-    path("auth/", include("auth_app.urls")),
+    path("api/v1/", include("api.urls")),
     path("", include("auth_app.urls")),
 ]
