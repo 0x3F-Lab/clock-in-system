e002e7201092c93b78a0c402f5081a1100c76087
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2025-06-08 18:05:38 +0800

    Upgrade static cache version - Add new js file for manage store page to SW to precache. - Upgrade static cache version to the next minor revision of `1.1.2`.

--- src/django/clock_in_system/settings.py
+++ src/django/clock_in_system/settings.py
@@ -23,7 +23,7 @@ def str_to_bool(value):
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
 ######################################################
 #          PLEASE CHANGE THIS EVERY VERSION          #
-STATIC_CACHE_VER = "v1.1.1"  #
+STATIC_CACHE_VER = "v1.1.2"  #
 #  Must be increased for any change to static files  #
 ######################################################
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
